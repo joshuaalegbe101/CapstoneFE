dfd55acb33f58876a8e586f53355862c08c98d34
joshuaalegbe101 <joshuaalegbe101@gmail.com>
2025-02-27 04:17:18 -0600

    Auth context

---
+++ src/context/AuthContext.jsx
@@ -0,0 +1,33 @@
+import { createContext, useState, useEffect } from "react";
+import { loginUser, registerUser } from "../services/api";
+import { getToken, saveToken, removeToken } from "../utils/auth";
+
+export const AuthContext = createContext();
+
+export const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const token = getToken();
+    if (token) {
+      setUser({ token }); // Decode for more info if needed
+    }
+  }, []);
+
+  const login = async (email, password) => {
+    const res = await loginUser({ email, password });
+    saveToken(res.data.token);
+    setUser(res.data);
+  };
+
+  const logout = () => {
+    removeToken();
+    setUser(null);
+  };
+
+  return (
+    <AuthContext.Provider value={{ user, login, logout }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
