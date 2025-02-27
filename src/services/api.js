c312e9e1be4a2f513dab79e2a66d2400472264e4
joshuaalegbe101 <joshuaalegbe101@gmail.com>
2025-02-27 06:52:10 -0600

    Api fix

--- src/services/api.js
+++ src/services/api.js
@@ -7,3 +7,4 @@ export const loginUser = (userData) => axios.post(`${API_URL}/auth/login`, userD
 export const fetchTransactions = (token) => axios.get(`${API_URL}/transactions`, { headers: { Authorization: `Bearer ${token}` } });
 export const fetchBudgets = (token) => axios.get(`${API_URL}/budgets`, { headers: { Authorization: `Bearer ${token}` } });
 export const createTransaction = (token, data) => axios.post(`${API_URL}/transactions`, data, { headers: { Authorization: `Bearer ${token}` } });
+export const createBudgets = (token) => axios.post(`${API_URL}/transactions`, {headers: { Authorization: `Bearer ${token}` } });
